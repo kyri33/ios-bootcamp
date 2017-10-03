5aba9459efb68dd215387170675ebfc39752b657
Kyri IOULIANOU <kioulian@e4r13p3.wethinkcode.co.za>
2017-10-03 18:54:24 +0200

    AWE

---
+++ d02/ex03/Table View/AddMemberViewController.swift
@@ -0,0 +1,53 @@
+//
+//  AddMemberViewController.swift
+//  Table View
+//
+//  Created by Kyri IOULIANOU on 2017/10/03.
+//  Copyright © 2017 Kyri IOULIANOU. All rights reserved.
+//
+
+import UIKit
+import os.log
+
+class AddMemberViewController: UIViewController {
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var dateField: UIDatePicker!
+    @IBOutlet weak var descriptionField: UITextView!
+    @IBOutlet weak var doneButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        guard let button = sender as? UIBarButtonItem, button === doneButton else {
+            print("Not saved, cancelling")
+            return
+        }
+        
+        let name = nameField.text
+        let description = descriptionField.text
+        let date = dateField.date
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ d02/ex03/Table View/TableViewController.swift
@@ -0,0 +1,61 @@
+//
+//  TableViewController.swift
+//  Table View
+//
+//  Created by Kyri IOULIANOU on 2017/10/03.
+//  Copyright © 2017 Kyri IOULIANOU. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    let names:[String] = [
+        "Hugh Hefner", "Chester Bennington", "Michael Jackson"
+    ]
+    
+    let descript:[String] = [
+        "Founder Of Playboy", "Singer For Linkin Park", "King of Pop"
+    ]
+    
+    let dates:[String] = [
+        "2017", "2017", "2010"
+    ]
+    
+    let cellSpacing: CGFloat = 20
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    @IBAction func unwindDeathList(sender: UIStoryboardSegue) {
+        if let sourceViewController = sender.source as? AddMemberViewController, let name = sourceViewController.name ?? "", let description = sourceViewController.description ?? "",
+            let date = sourceViewController.date {
+            
+            let newIndexPath = IndexPath(row: names.count, section: 0)
+            names.append(name)
+            descript.append(name)
+            let dateFormatr = DateFormatter()
+            dateFormatr.dateFormat = "dd-MM-YYYY"
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let row = indexPath.row
+        cell.textLabel?.text = names[row] + " - " + dates[row]
+        cell.detailTextLabel?.text = descript[row]
+        return cell
+    }
+    
+
+}
