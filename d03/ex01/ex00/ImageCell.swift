552b85c7f3aaad2e3d0f45d4951ddff17af5d3e5
Kyri IOULIANOU <kioulian@e4r12p3.wethinkcode.co.za>
2017-10-06 16:13:13 +0200

    I'm too good malaka

---
+++ d03/ex01/ex00/ImageCell.swift
@@ -0,0 +1,16 @@
+//
+//  ImageCell.swift
+//  ex00
+//
+//  Created by Kyri IOULIANOU on 2017/10/06.
+//  Copyright © 2017 Kyri IOULIANOU. All rights reserved.
+//
+
+import UIKit
+
+class ImageCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
