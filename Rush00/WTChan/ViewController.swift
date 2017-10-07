3f3818b8ff547d946f5cff4cf0865f78ee9ae076
Kyri IOULIANOU <kioulian@e4r11p6.wethinkcode.co.za>
2017-10-07 14:32:36 +0200

    Awe

---
+++ Rush00/WTChan/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  WTChan
+//
+//  Created by Kyri IOULIANOU on 2017/10/07.
+//  Copyright © 2017 Kyri IOULIANOU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let UID = "dc02ac6b227a3dfbddc79673fd66af056fdcafd2f291fced522166e201ba4492"
+    let secret = "2a90c54653843bf1ac25eeaa309d856028a3d418663105e3f9a36aefb1154c58"
+    var token: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        userAuthenticate()
+        fetchToken()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+    }
+    
+    func userAuthenticate() {
+        let urlStr = "https://api.intra.42.fr/oauth/authorize?client_id=\(self.UID)&redirect_uri=http%3A%2F%2Fwww.mibrandapp.com&response_type=code&scope=public%20forum"
+        if let url = URL(string: urlStr) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        } else {
+            print("URL Error")
+        }
+        
+    }
+    
+    func fetchToken() {
+        let url = URL(string: "https://api.intra.42.fr/oauth/token")
+        let session = UserDefaults.standard
+        var request = URLRequest(url: url!)
+        request.httpMethod = "POST"
+        request.setValue("application/x-www-form-urlencoded;charset=UTF-8", forHTTPHeaderField: "Content-Type")
+        request.httpBody = "grant_type=client_credentials&client_id=\(self.UID)&client_secret=\(self.secret)".data(using: String.Encoding.utf8)
+        let task = URLSession.shared.dataTask(with: request as URLRequest) {
+            (data, response, error) in
+            if let err = error {
+                print(err)
+            } else if let d = data {
+                do {
+                    if let dic: NSDictionary = try JSONSerialization.jsonObject(with: d, options: .mutableContainers) as? NSDictionary {
+                        if let response = dic["access_token"] {
+                            self.token = response as? String
+                            session.set(dic.value(forKey: "access_token"), forKey: "token")
+                            session.synchronize()
+                            print("Token " + self.token!)
+                        } else {
+                            print("token error")
+                        }
+                    }
+                } catch (let err) {
+                    print(err)
+                }
+            }
+        }
+        task.resume()
+    }
+
+
+}
+
